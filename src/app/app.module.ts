6a736107ae898fb5a946aeb8a77003d4e8acb98b
Elohina Guevara <elohinaguevara@gmail.com>
2017-09-19 00:34:26 -0400

    First commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
+import {MdToolbarModule} from '@angular/material';
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MdToolbarModule
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MdToolbarModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
