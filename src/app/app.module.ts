54ab6f12544e2fd1dd7e852b69d89d2fa31c14dd
Elohina Guevara <elohinaguevara@gmail.com>
2017-09-23 23:09:11 -0400

    Add components to app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
-import {MdToolbarModule} from '@angular/material';
-
 import { AppComponent } from './app.component';
 
+import { CoreModule } from './core/core.module';
+import { HomeComponent } from './home/home.component';
+import { ProductsComponent } from './products/products.component';
 @NgModule({
   declarations: [
     AppComponent,
-    MdToolbarModule
+    HomeComponent,
+    ProductsComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MdToolbarModule
+    CoreModule
+  ],
+  exports: [
+    AppComponent,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/core/core.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+// import {MdToolbarModule} from '@angular/material';
+
+@NgModule({
+  declarations: [
+  ],
+  imports: [
+    CommonModule
+  ],
+  providers: []
+})
+export class CoreModule { }
